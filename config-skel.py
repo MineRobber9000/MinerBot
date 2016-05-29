0d82d405ba2c34a9a3a0d9e709e1fba74fc50b2d
MineRobber9000 <kd2bwz@yahoo.com>
2016-05-28 20:30:45 -0400

    Code for today (NOT WORKING ATM)

--- config-skel.py
+++ config-skel.py
@@ -4,5 +4,9 @@ APP_ID = ""
 APP_SECRET = ""
 # Refresh token
 REFRESH_TOKEN = ""
+# User agent (used in the creation of the reddit client object)
+USER_AGENT = ""
 # Bot account username (used in commands, i.e; "/u/blahbot hello")
 USERNAME = ""
+# Scopes needed (don't change unless upgrading to new version, bot code requires these)
+SCOPES = ["identity","read","submit","privatemessages"]
